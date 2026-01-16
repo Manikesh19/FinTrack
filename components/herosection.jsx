992945bbbbfd0c8024065ade2c090b2d2bbaf42f
Manikesh <manikesh19dec@gmail.com>
2026-01-16 23:51:23 +0530

    Finishing homepage Ui

--- components/herosection.jsx
+++ components/herosection.jsx
@@ -34,7 +34,7 @@ const HeroSection = () => {
                 <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
                     An AI-powered financial management system that helps you track, analyze, and optimize your spending with realtime insights. 
                 </p>
-                <div className="flex justify-center space-x-4">
+                <div className="flex justify-center space-x-4 mb-10">
                     <Link href="/dashboard">
                         <Button size="lg" className="px-8">
                             Get Started
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "/api/portraits/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
