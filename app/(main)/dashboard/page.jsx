aa1b1c2b06f5e69ce9fc2fe61fa8cdd2d6fc1ea4
Manikesh <manikesh19dec@gmail.com>
2026-01-27 00:42:33 +0530

    setting up create account feature

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,8 +1,26 @@
-
-const Dashboard = () => {
+import CreateAccountDrawer from "@/components/create-account-drawer";
+import { Card, CardContent } from "@/components/ui/card";
+import { Plus } from "lucide-react";
+const DashboardPage = () => {
     return (
-        <div>Dashboard Page</div>
+        <div className="px-5">
+             {/* Budget Progress */}
+
+             {/* Overview */}
+
+             {/* Accounts Grid */}
+             <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+                <CreateAccountDrawer>
+                    <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
+                        <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
+                            <Plus className="h-10 w-10 mb-2"/>
+                            <p className="text-small font-medium">Add new account</p>
+                        </CardContent>
+                    </Card>
+                </CreateAccountDrawer>
+             </div>
+        </div>
     )
 }
 
-export default Dashboard;
+export default DashboardPage;
